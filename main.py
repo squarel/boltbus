a3c54a6a0074cb5ff56550ef5f1d45383bfd813d
squarel <squarelnix@gmail.com>
2013-05-11 22:42:38 -0400

    view state

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import requests
-import pymongo
+from pymongo import MongoClient
 from datetime import date, timedelta
 from bs4 import BeautifulSoup
 
 # initial variables
 stations = ['Boston South Station - Gate 9 NYC-Gate 10 NWK/PHL',
-           'Philadelphia JFK & N. 30th St',
-           'New York 34th and 8th by Tick Tock (Phil. or Bos.)',]
+           'New York 34th and 8th by Tick Tock (Phil. or Bos.)',
+           'Philadelphia JFK & N. 30th St']
 
 max_days = 7
 
-client = MongoClient('localhost','20017')
+client = MongoClient('localhost', 27017)
 db = client.boltbus
 collection = db.shifts
 
@@ -20,52 +20,85 @@ def next_days(date, days):
 
 def get_content(depart, dest, date):
     s = requests.session()
-    s.get('https://www.boltbus.com')
+    r = s.get('https://www.boltbus.com')
 
-    f = open('conf.txt', 'r')
+    f = open('conf2.txt', 'r')
     lines = f.readlines()
     post_data = {}
     for line in lines:
         strs = line.rstrip().rsplit(':')
         post_data[strs[0]] = strs[1]
+
+
+    soup = BeautifulSoup(r.content)
+    event_validation = soup.find('input',id='__EVENTVALIDATION')
+    post_data['__EVENTVALIDATION'] = event_validation.attrs['value']
+    viewstate = soup.find('input',id='__VIEWSTATE')
+    post_data['__VIEWSTATE'] = viewstate.attrs['value']
+    #toolkit = soup.find('input',id='ctl00_toolkitScriptManager_HiddenField')
+    post_data['ctl00_toolkitScriptManager_HiddenField'] = ';;AjaxControlToolkit, Version=1.0.20229.20821, Culture=neutral, PublicKeyToken=28f01b0e84b6d53e:en-US:c5c982cc-4942-4683-9b48-c2c58277700f:e2e86ef9:1df13a87;;AjaxControlToolkit, Version=1.0.20229.20821, Culture=neutral, PublicKeyToken=28f01b0e84b6d53e:en-US:c5c982cc-4942-4683-9b48-c2c58277700f:c4c00916:9ea3f0e2:9e8e87e9:4c9865be:ba594826:c76f1358:182913ba:bae32fb7:8ccd9c1b:3858419b:96741c43:c7c04611:cd120801:38ec41c0:a6a5a927;'
+
     post_data['ctl00$cphM$forwardRouteUC$lstDestination$textBox'] = dest
     post_data['ctl00$cphM$forwardRouteUC$lstOrigin$textBox'] = depart
     post_data['ctl00$cphM$forwardRouteUC$txtDepartureDate'] = date
-    r = s.post('https://www.boltbus.com', data=post_data)
+    r = s.post('https://www.boltbus.com/Default.aspx', data=post_data, allow_redirects=True)
+    #print post_data['__EVENTVALIDATION']
+    #print s.cookies
+
+    #f2 = open('page.html','w')
+    #f2.write(r.content)
 
     return r.content
 
 def create_record(depart, dest, date, content):
     record = {}
-    tickets = {}
-    record['time']
+    tickets = []
+    record['date'] = date.strftime('%m/%d/%y')
     record['depart'] = depart
     record['dest'] = dest
 
     soup = BeautifulSoup(content)
-    tickets['fare'] = soup.find_all('td','faresColumn0')
-    tickets['start'] = soup.find_all('td','faresColumn1')
-    tickets['end'] = soup.find_all('td','faresColumn2')
+    fares = soup.find_all('td','faresColumn0')
+    starts =  soup.find_all('td','faresColumn1')
+    ends = soup.find_all('td','faresColumn2')
+    for i in range(len(fares)):
+        ticket = {}
+        ticket['fare'] = fares[i].getText().strip()
+        ticket['start'] = starts[i].getText().strip()
+        ticket['ends'] = ends[i].getText().strip()
+        tickets.append(ticket)
+    record['tickets'] = tickets
 
+    print record
+
+def have_content(content):
+    soup = BeautifulSoup(content)
+    fare = soup.find_all('td','faresColumn0')
+    if fare == []:
+        return False
+    else:
+        return True
 
 
 def create_records(depart, dest):
     # date
     today = date.today()
-    flag = True
+    crawl_flag = True
     next_day = 0
-    while True:
+    no_days = 0
+    while crawl_flag:
         d = next_days(today, next_day)
         date_str = d.strftime('%m/%d/%y')
         content = get_content(depart, dest, date_str)
+        if have_content(content):
+            create_record(depart, dest, d, content)
+            no_days = 0
+        else:
+            no_days += 1
+            print "no_days:" + str(no_days)
         next_day += 1
-
-
-    for d in range(0, max_days + 1):
-
-
-
-
+        if no_days >= max_days:
+            break
 
 
 if __name__ == "__main__":
